6123a1ca245da75d456769c51312871bfe9d75ff
Tiago Villa <tiago_villa@hotmail.com>
2022-07-17 13:27:41 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar />
+  <router-view></router-view>
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: { Navbar}
+}
+</script>
+
 <style>
+body {
+  background: #f2f2f2;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  max-width: 600px;
+  margin: 0 auto;
   color: #2c3e50;
 }
 
